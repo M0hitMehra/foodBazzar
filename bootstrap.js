1890e26a598106db7594b76d63ad79965488ec1e
M0hitMehra <98013205+M0hitMehra@users.noreply.github.com>
2022-03-06 10:12:42 +0530

    Add files via upload

---
+++ bootstrap.js
@@ -0,0 +1,12 @@
+function bigImg(x) {
+    let p = document.querySelectorAll(".l");
+
+    p.style.visibility = "visible";
+    
+  }
+  
+  function normalImg(x) {
+    let p = document.querySelector(".l");
+    p.style.visibility = "hidden";
+    
+  }
